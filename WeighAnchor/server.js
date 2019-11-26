19d5d446ef5ef68aa9cb2691a989cd90d74bf391
Niels Larmuseau <nielslarmuseau@gmail.com>
2019-11-26 21:41:00 +0100

    fixes

--- WeighAnchor/server.js
+++ WeighAnchor/server.js
@@ -1,11 +1,12 @@
-
 const http = require('http');
 const WebSocket = require("ws");
 const fs = require("fs");
 const url = require('url');
+const path = require('path');
 const WebPort = process.env.PORT || 1337;
 const classes = require("./js/serverClasses");
 const WebSocketPort = Number(process.env.WSPORT || 6440);
+const ServerIP = process.env.SERVERIP || "localhost";
 const ticktime = 25;
 const wss = new WebSocket.Server({
     port: WebSocketPort
@@ -13,19 +14,45 @@ const wss = new WebSocket.Server({
 
 var region;
 
+// maps file extention to MIME types
+const mimeType = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword',
+    '.eot': 'appliaction/vnd.ms-fontobject',
+    '.ttf': 'aplication/font-sfnt'
+};
 
 http.createServer(function (req, res) {
     let pathName = url.parse(req.url).pathname;
-    if (pathName === "/") { pathName = "/index.html";}
-    fs.readFile("./client" + pathName, function (error, data) {
+    if (pathName === "/") {
+        pathName = "/index.html";
+    }
+    //if (fs.statSync(pathName).isDirectory()) {
+    //    pathName += '/index.html';
+    //}
+    fs.readFile("./client" + pathName,  function (error, data) {
         if (error) {
             res.writeHead(404);
             //res.write(error);
             res.end();
         } else {
+            const ext = path.parse(pathName).ext;
             res.writeHead(200, {
-                'Content-Type': 'text/html'
+                'Content-Type': mimeType[ext] || 'text/plain'
             });
+            if (pathName === "/index.html" ) {
+                data = data.toString().replace("#ServerIP#", ServerIP).replace("#WebSocketPort#", WebSocketPort);
+            }
             res.write(data);
             res.end();
         }
@@ -61,7 +88,7 @@ function initConnection() {
         //Player Joining
         socket.on("message", function incoming(msg) {
             msgobj = JSON.parse(msg);
-            if (msgobj.action == "ping") {
+            if (msgobj.action === "ping") {
                 //ignore
             } //for testing T001
             //console.log('received: %s', message); // Debugging
